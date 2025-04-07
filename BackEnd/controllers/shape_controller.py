1b28c40659c2629ae836dfa277ba1d2cb3196205
Tung Nguyen X. <xuntgn12@gmail.com>
2025-04-07 14:07:25 +0700

    3rd commit April 7th 2025: - extracting the layer validation logic into a private helper method to reduce code duplication

--- BackEnd/controllers/shape_controller.py
+++ BackEnd/controllers/shape_controller.py
@@ -6,22 +6,21 @@ class ShapeController:
         """Initialize the shape controller with a reference to the layer controller."""
         self.layer_controller = layer_controller
 
+    def _validate_layer(self, layer):
+        return layer in self.layer_controller.layers and isinstance(layer.object, ShapeModel)
+
     def set_line_color(self, layer, color):
-        """Set the line color of a shape in the layer."""
-        if layer in self.layer_controller.layers and isinstance(layer.object, ShapeModel):
+        if self._validate_layer(layer):
             layer.object.set_line_color(color)
 
     def set_fill_color(self, layer, color):
-        """Set the fill color of a shape in the layer."""
-        if layer in self.layer_controller.layers and isinstance(layer.object, ShapeModel):
+        if self._validate_layer(layer):
             layer.object.set_fill_color(color)
 
     def set_border_radius(self, layer, radius):
-        """Set the border radius of a shape in the layer."""
-        if layer in self.layer_controller.layers and isinstance(layer.object, ShapeModel):
+        if self._validate_layer(layer):
             layer.object.set_border_radius(radius)
 
     def set_alpha(self, layer, alpha):
-        """Set the transparency of a shape in the layer."""
-        if layer in self.layer_controller.layers and isinstance(layer.object, ShapeModel):
+        if self._validate_layer(layer):
             layer.object.set_alpha(alpha)
